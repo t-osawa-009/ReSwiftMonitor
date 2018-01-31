60ccd2303e04b1389cca8b7eddbb38688f40e2c8
takuchantuyoshi <da87435@gmail.com>
2018-02-01 08:39:10 +0900

    Initial Commit

---
+++ ReSwiftMonitor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReSwiftMonitor
+//
+//  Created by 大澤卓也 on 2018/02/01.
+//  Copyright © 2018年 Takuya Ohsawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
